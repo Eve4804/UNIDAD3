2ef81e549100ec3490d37e87f4292cae31448540
Eve4804 <143903874+Eve4804@users.noreply.github.com>
2023-09-26 16:27:37 -0600

    Add files via upload

---
+++ NumeroParImpar.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numeroparimpar;
+
+import java.util.Scanner;
+
+public class NumeroParImpar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner numero = new Scanner(System.in);
+        System.out.println("INGRESA UN NUMERO");
+        int ParImpar = numero.nextInt();
+        
+        if(ParImpar% 2 == 0){
+            System.out.println("EL NUMERO "+ ParImpar+ " ES PAR");
+        } else {
+            System.out.println("El numero "+ParImpar+ " es impar" );
+        }
+        
+    
+    }
+    }
+    
+
